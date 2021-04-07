f38d3fb03c977716eb64bc0ca898a095026b37b8
brl4528 <blgc.sgo@hotmail.com>
2021-04-07 08:45:50 -0400

    create component header, signInbutton and Subscrib

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../service/stripe';
 
 import styles from './home.module.scss';
 
-export default function Home() {
+interface HomeProps {
+  products: {
+    priceId: string;
+    amount: number;
+  };
+}
+
+export default function Home({ products }: HomeProps) {
   return (
     <>
       <Head>
@@ -18,10 +27,10 @@ export default function Home() {
           </h1>
           <p>
             Get access to all the publications <br />
-            <span>for $9.90 month</span>
+            <span>for {products.amount} month</span>
           </p>
 
-          <SubscribeButton />
+          <SubscribeButton priceId={products.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
@@ -29,3 +38,21 @@ export default function Home() {
     </>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IdXJyHj7boZm8gcr1XCiFv7');
+
+  const products = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  };
+  return {
+    props: {
+      products,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+  };
+};
