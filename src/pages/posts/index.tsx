407bcceb8366c6edf34636ff1306d7c1fec29807
brl4528 <blgc.sgo@hotmail.com>
2021-05-26 14:47:34 -0400

    update

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,38 +1,70 @@
 import Head from 'next/head';
 import styles from './styles.module.scss';
 
+import Prismic from '@prismicio/client';
+import { getPrismicClient } from '../../service/prismic';
+import { GetStaticProps } from 'next';
+
 export default function Posts() {
   return (
     <>
-     <Head>
-       <title>Posts | Ignews</title>
-     </Head>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-
-     <main className={styles.container}>
-       <div className={styles.posts}>
-         <a>
-           <time>12 de março de 2021</time>
-           <strong>Creating a Modoihcfo sabhdisb</strong>
-           <p>oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma</p>
-         </a>
-         <a>
-           <time>12 de março de 2021</time>
-           <strong>Creating a Modoihcfo sabhdisb</strong>
-           <p>oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma</p>
-         </a>
-         <a>
-           <time>12 de março de 2021</time>
-           <strong>Creating a Modoihcfo sabhdisb</strong>
-           <p>oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma</p>
-         </a>
-         <a>
-           <time>12 de março de 2021</time>
-           <strong>Creating a Modoihcfo sabhdisb</strong>
-           <p>oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma</p>
-         </a>
-       </div>
-     </main>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Modoihcfo sabhdisb</strong>
+            <p>
+              oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s
+              dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Modoihcfo sabhdisb</strong>
+            <p>
+              oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s
+              dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Modoihcfo sabhdisb</strong>
+            <p>
+              oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s
+              dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma
+            </p>
+          </a>
+          <a href="#">
+            <time>12 de março de 2021</time>
+            <strong>Creating a Modoihcfo sabhdisb</strong>
+            <p>
+              oidewidfe dsoijisojc wejdiojwd d d edowijdosnd wsd sad pwqeojdsd s
+              dsd wepqokd s wokdkmsamsd w dsapokzlsdxçlma
+            </p>
+          </a>
+        </div>
+      </main>
     </>
-  )
+  );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query(
+    [Prismic.predicates.at('document.type', 'publication')],
+    {
+      fetch: ['publication.title', 'publication.content'],
+      pageSize: 100,
+    }
+  );
+
+  console.log(response);
+  return {
+    props: {},
+  };
+};
